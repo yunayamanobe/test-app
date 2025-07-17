eabe5535905d196b50bc1f33f7ba418219f7abe1
yunayamanobe <148517467+yunayamanobe@users.noreply.github.com>
2025-07-17 18:40:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function fortune() {
+  const animals = ["ウーパールーパー", "大カラス", "ハリネズミ", "黒猫", "銀蛇"];
+  const fortunes = [
+    "パワー全開大吉！今日は最高の一日になるでしょう！水辺を歩くとさらに最高になります！",
+    "中吉！なにかいいことがあるかも！でも画鋲は踏みます！気を付けてください！",
+    "小吉。ちょっとだけ良い日！でもコンビニに行くとランチは全部売り切れてます！サイゼリヤに行きましょう！",
+    "吉。穏やかに過ごせそう。でも仕事は忙しいです。仕方ないよね。",
+    "凶...セブンイレブンのおにぎりクーポンをまた使い忘れちゃったね…今日はおとなしくしてよう。"
+  ];
+
+  const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+
+  const result = `${randomAnimal}が言いました：「${randomFortune}」`;
+  document.getElementById("result").innerText = result;
+}
